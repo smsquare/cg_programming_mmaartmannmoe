67ae231ba546cd0e74af6a9f4b9665a3cfc5267d
unknown <r202-stu@AI241LAB0202P04.aipdcls.edu>
2015-04-13 18:43:18 -0700

    Draw first triangle

--- cg_programming_mMaartmannMoe/main.cpp
+++ cg_programming_mMaartmannMoe/main.cpp
@@ -16,12 +16,21 @@
 
 using namespace glm;
 
+// An array of 3 vectors which represents 3 vertices
+static const GLfloat g_vertex_buffer_data[] = {
+	-1.0f, -1.0f, 0.0f,
+	1.0f, -1.0f, 0.0f,
+	0.0f, 1.0f, 0.0f
+};
+
 int main() {
 
 	/* Initialize the library */
 	if (!glfwInit()) {
 		fprintf( stderr, "Failed to initialize GLFW\n");
 		return -1;
+	} else {
+		fprintf(stdout, "Initialized GLFW\n");
 	}
 
 	// Define some properties of the window being created...
@@ -35,29 +44,68 @@ int main() {
 	GLFWwindow* window;
 	window = glfwCreateWindow(1024, 768, "Main Window", NULL, NULL);
 	if (window == NULL) {
-		fprintf(stderr, "Failed to open GLFW window.");
+		fprintf(stderr, "ERROR: Failed to open GLFW window.\n");
 		glfwTerminate();
 		return -1;
+	} else {
+		fprintf(stdout, "Created window...\n");
 	}
 
 	/* Make the window's context current*/
 	// Initialize GLEW
 	glfwMakeContextCurrent(window);
 	glewExperimental = true; // Needed in core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		/* Problem: glewInit failed!!! */
+		fprintf(stderr, "ERROR %s: Failed to initialize GLEW.\n", glewGetErrorString(err));
 		return -1;
+	} else {
+		fprintf(stdout, "Initialized GLEW...\n");
 	}
 
-	/* Loop until the user closes the window */
+	///////////////////////// WEEK 02 /////////////////////////////////////////////////////////////////////
+	// Create a Vertex Array Object; Set as current one...												 //
+	GLuint VertexArrayID;																				 //
+	glGenVertexArrays(1, &VertexArrayID);																 //
+	glBindVertexArray(VertexArrayID);																	 //
+																										 //
+	// This will identify our vertex buffer																 //
+	GLuint vertexBuffer;																				 //
+																										 //
+	// Generate 1 buffer, put the resulting identifier in vertexBuffer									 //
+	glGenBuffers(1, &vertexBuffer);																		 //
+																										 //
+	// Bind the vertexBuffer...																			 //
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);														 //
+																										 //
+	// Give vertices to OpenGL...																		 //
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);	 //
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	/* Loop until the user closes the window or presses ESC */
 	do {
 		/* Render Here */
+		// 1st attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexAttribPointer(
+			0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0
+		);
+
+		// Draw the triangle!
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0
+		glDisableVertexAttribArray(0);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			fprintf(stdout, "ESC pressed! Exiting program!\n");
+		}
 
 	} // Check if the ESC key was pressed OR the window was closed...
 	while ( 
